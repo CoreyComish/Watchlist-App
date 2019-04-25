2681a2a917a2c3050ba802f5884f4c54f8c6a1a2
Corey Comish <ccomish@uoregon.edu>
2019-04-24 22:20:28 -0700

    made it so button findsimage of text field

--- Watchlist/ViewController.swift
+++ Watchlist/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var movieTitleTF: UITextField!
     @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         TMDBConfig.apikey = "ac6045995596442d0ad6f068b91f0cee"
         movieTitleTF.delegate = self
-        getMovie()
     }
     
     // MARK: UITextFieldDelegate
@@ -36,19 +36,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        movieTitleLabel.text = "Default Text"
+        movieTitleLabel.text = movieTitleTF.text;
+        SearchMDB.movie(query: movieTitleTF.text!, language: "en", page: 1, includeAdult: true, year: 0, primaryReleaseYear: 0){
+            data, wrds in
+            let search = wrds?[0].id;
+            self.setImage(id: search!);
+        }
     }
     
-    func getMovie() {
-        MovieMDB.movie(movieID: 7984, language: "en"){
-            apiReturn, movie in
-            if let movie = movie{
-                print(movie.title)
-                print(movie.revenue)
-                print(movie.genres[0].name)
-                print(movie.production_companies?[0].name)
-                self.movieTitleLabel.text = movie.title!
+    func setImage(id: Int) {
+        MovieMDB.images(movieID: id, language: "en") {
+            data, imgs in
+            if let images = imgs{
+                let imgUrl = images.posters[0].file_path!;
+                let urlString = "http://image.tmdb.org/t/p/w500" + imgUrl;
+                guard let url = URL(string: urlString) else { return }
+                URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if error != nil {
+                        print("Failed fetching image:", error!)
+                        return
+                    }
+                    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                        print("Not a proper HTTPURLResponse or statusCode")
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        self.movieImage.image = UIImage(data: data!)
+                    }
+                    }.resume()
             }
         }
     }
+    
 }
