aaf0528263a98b3f2640886a51cc394e01135d7b
Corey Comish <ccomish@uoregon.edu>
2019-04-29 20:13:37 -0700

    Working on Tables

---
+++ Watchlist/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  Watchlist
+//
+//  Created by Corey Comish on 4/29/19.
+//  Copyright © 2019 Corey Comish. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
